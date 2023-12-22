90e8ad5751ec17343f81dcba56c1b217804c6ee1
Anastasia S <aparicion@mail.ru>
2023-12-22 22:10:43 +0300

    Commit_1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package Task7_finalWork;
+
+import Task7_finalWork.controller.CalculatorController;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorController calculator = new CalculatorController();
+        calculator.start();
+    }
+
+}
